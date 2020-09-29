cfb871b4555f39bb42ab7ce5797507a01773ab10
Ted Sikora <tsikora@nacdonline.org>
2020-09-29 10:39:21 -0400

    Add ts_agreement_plot and ts_agreement_prep

---
+++ R/ts_agreement_plot.R
@@ -0,0 +1,52 @@
+#' Creates graph for multiple item agreement questions
+#'
+#' \code{ts_agreement_plot} Given a tibble of a given multiple level agreement question,
+#'     creates graph.
+#'
+#' This is an NACD function: Should you experience difficulties, see Ted Sikora
+#'
+#' @param .df A dataframe, piped from \code{ts_agreement_prep}
+#' @param .cap Text for a caption (optional)
+#' @return A bar graph
+#' @return A graph
+#'
+#' @examples
+#' ts_agreement_prep(df, dd, Q27, c("Agree", "Neither agree nor disagree", "Disagree")) %>%
+#' ts_agreement_plot()
+#'
+#'
+ts_agreement_plot <- function (.df, .cap = NULL){
+  title <- .df %>% dplyr::select(title) %>% dplyr::pull()
+  .p <- .df %>%
+    ggplot2::ggplot(ggplot2::aes(x = forcats::fct_reorder(labs,-temp3), y = Percent, fill = value)) +
+    ggplot2::geom_col(position = ggplot2::position_fill(), width = 0.5) +
+    ggplot2::coord_flip() +
+    ggplot2::scale_y_continuous(labels = NULL, expand = ggplot2::expand_scale(add = c(0, 0), mult = c(0, 0))) +
+    ggplot2::scale_x_discrete(labels = function(xvar) stringr::str_wrap(xvar, 60), expand = ggplot2::expand_scale(add = c(0.1, 0.1))) +
+    ggplot2::scale_fill_manual(values = c("#21578e", "#a7a9ac", "#c15927"), guide = ggplot2::guide_legend(reverse = TRUE)) +
+    ggplot2::geom_text(ggplot2::aes(label = scales::number(Percent, scale = 100, accuracy = 1), y = Percent), fontface = "bold", color = "#ffffff", size = 3.5, position = ggplot2::position_fill(vjust = 0.5)) +
+    ggplot2::labs(x = NULL, y = NULL, title = stringr::str_wrap(.df$title[[1]], width = ifelse(stringr::str_length(.df$title[[1]] > 60), stringr::str_length(.df$title[[1]])/1.8, 60)), caption = ifelse(is.null(.cap), NA, .cap)) +
+    ggplot2::theme(plot.title    = ggplot2::element_text(hjust = 0.5, size = 12, margin = ggplot2::margin(0.05, 5, 0.5, 0, "cm"), face = "bold"),
+                   plot.subtitle = ggplot2::element_blank(),
+                   plot.caption  = ggplot2::element_text(size = ggplot2::rel(0.4)),
+                   plot.margin   = ggplot2::margin(0.5, 0, 0.25, 1, "cm"),
+
+                   legend.title  = ggplot2::element_blank(),
+                   legend.text   = ggplot2::element_text(size = ggplot2::rel(0.6)),
+                   legend.key    = ggplot2::element_rect(size = 0.5),
+                   legend.key.size = ggplot2::unit(0.3, "cm"),
+                   legend.margin = ggplot2::margin(0, 0, 0, 0, "cm"),
+                   legend.position = "top",
+
+                   axis.title  = ggplot2::element_blank(),
+                   axis.line   = ggplot2::element_blank(),
+                   axis.ticks  = ggplot2::element_blank(),
+                   axis.text.y = ggplot2::element_text(size = ggplot2::rel(0.95)))
+  if (is.null(.cap)) {
+    .p <- .p + ggplot2::theme(plot.caption = ggplot2::element_blank())
+  }
+  else {
+    .p <- .p
+  }
+  .p
+}
---
+++ R/ts_agreement_prep.R
@@ -0,0 +1,43 @@
+#' Prepares data for multiple item agreement questions
+#'
+#' \code{ts_agreement_prep} returns a tibble of a given multiple level agreement question,
+#'     prepared for tables or graphs.
+#'
+#' This is an NACD function: Should you experience difficulties, see Ted Sikora
+#'
+#' @param .df An imported dataframe
+#' @param .dd A data dictionary
+#' @param .x A question number
+#' @param .l The three level names
+#' @return A graph
+#'
+#' @examples
+#' df %>% newfun(dd, Q8)
+#'
+#'
+ts_agreement_prep <- function (.df, .dd, .x, .l){
+  myround <- function(x) trunc(x+0.51)
+  v <- rlang::enquo(.x)
+  q <- rlang::quo_name(v)
+  labs <- tibble::tibble(title = .dd[.dd$qnames == q, c("title")][[1]], labs = .dd[.dd$qnames == q, c("value")][[1]], qvar = .dd[.dd$qnames == q, c("name")][[1]])
+  if (!(missing(.l))) {
+    .temp <- .df %>%
+      dplyr::select(dplyr::contains(q, ignore.case = FALSE)) %>%
+      tidyr::gather() %>%
+      mutate(value = factor(value, c("Strongly agree", "Agree", "Neither agree nor disagree", "Disagree", "Strongly disagree"))) %>%
+      mutate(value = fct_collapse(value, Agree = c("Strongly agree", "Agree"), `Neither agree nor disagree` =  c("Neither agree nor disagree"), Disagree = c("Disagree", "Strongly disagree"))) %>%
+      dplyr::add_count(key, value) %>%
+      dplyr::distinct() %>%
+      dplyr::filter(!is.na(value)) %>%
+      dplyr::group_by(key) %>%
+      dplyr::mutate(tot = sum(n), Percent = n/tot) %>%
+      dplyr::select(key, value, tot,  Percent) %>%
+      dplyr::mutate_at(dplyr::vars(value),  ~factor(., levels = .l, ordered = TRUE)) %>%
+      dplyr::mutate(temp = as.integer(value), temp2 = temp*Percent) %>%
+      {dplyr::left_join(labs, ., by = c(qvar = "key"))} %>%
+      dplyr::group_by(labs) %>%
+      dplyr::mutate(temp3 = mean(temp2)) %>%
+      dplyr::ungroup()
+  }
+  .temp
+}
